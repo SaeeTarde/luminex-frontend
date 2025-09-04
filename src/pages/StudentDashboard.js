2327ad98400b53adbc0db1b7e2bc2eebb4ef08a6
Saee Tarde <saee131989@gmail.com>
2025-09-04 17:04:19 +0530

    Added Login Page

---
+++ src/pages/StudentDashboard.js
@@ -0,0 +1,5 @@
+// src/pages/StudentDashboard.js
+import React from "react";
+export default function StudentDashboard() {
+  return <h1>Welcome Student Dashboard 🎓</h1>;
+}
---
+++ src/pages/TeacherDashboard.js
@@ -0,0 +1,5 @@
+// src/pages/TeacherDashboard.js
+import React from "react";
+export default function TeacherDashboard() {
+  return <h1>Welcome Teacher Dashboard 👩‍🏫</h1>;
+}
